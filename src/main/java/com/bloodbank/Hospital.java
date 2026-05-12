73877accc4a9a5dca24ade711e9c34a80bd79102
shahxvi <2025171523@student.uitm.edu.my>
2026-05-12 09:56:02 +0800

    Add Hospital class

---
+++ src/main/java/com/bloodbank/Hospital.java
@@ -0,0 +1,48 @@
+// MIT LICENSE
+// Copyright (c) 2026 Shah
+
+public class Hospital {
+    private String name;
+    private String address;
+    private int contact;
+
+    public Hospital(String name, String address, int contact) {
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+    }
+
+    /* Setters */
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setAddress (String address) {
+        this.address = address;
+    }
+
+    public void setContact (int contact) {
+        this.contact = contact;
+    }
+    /* Setters */
+
+    /* Getters */
+    public String getName () {
+        return name;
+    }
+
+    public String getAddress () {
+        return address;
+    }
+
+    public int getContact () {
+        return contact;
+    }
+    /* Getters */
+
+    public String toString() {
+        return "Name: " + name +
+               "\nAddress: " + address +
+               "\nContact: " + contact;
+    }
+}
