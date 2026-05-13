c5470f966a0e8bf0c7ec3973d421bfa658c53766
itsIqball <iqbalmubin11@gmail.com>
2026-05-13 23:15:57 +0800

    complete Staff class

--- src/main/java/com/bloodbank/staff/Staff.java
+++ src/main/java/com/bloodbank/staff/Staff.java
@@ -1,7 +1,46 @@
 // MIT LICENSE
 // Copyright (c) 2026 Iqbal
+// Copyright (c) 2026 Shah
 
 package com.bloodbank.staff;
 
-public class Staff {
+import com.bloodbank.Person;
+
+public class Staff extends Person {
+	private int id;
+	private String password;
+	
+	public Staff(int nric, String name, int contact, int id, String password) {
+		super(nric, name, contact);
+		this.id = id;
+		this.password = password;
+	}
+	
+	/* Setters */
+	private void setID(int id) {
+		this.id = id;
+	}
+	
+	private void setPassword(int password) {
+		this.password = password;
+	}
+	/* Setters */
+	
+	public boolean verifyCredentials(int id, String password){
+		if(id == this.id && this.password.equals(password))
+			return true;
+		return false;
+	}
+	
+	/* Printers */
+	public String toString() {
+		return super.toString() +
+			   "\nID; " + id +
+			   "\nPassword " + password;
+	}
+	
+	public String toRecord() {
+		return super.toRecord() + "; " + id + "; " + password;
+	}
+	/* Printers */
 }
