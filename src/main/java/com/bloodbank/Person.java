d5322e3baa804c8043c7c6f7767e9e5300047e9b
shahxvi <2025171523@student.uitm.edu.my>
2026-05-13 21:24:46 +0800

    Edit Person Class

--- src/main/java/com/bloodbank/Person.java
+++ src/main/java/com/bloodbank/Person.java
@@ -1,58 +1,60 @@
 //MIT LICENSE
 //Copyright (c) 2026 Isya
-//Copyright (c) 2026 Maya	
+//Copyright (c) 2026 Maya   
+//Copyright (c) 2026 Shah   
 
 import com.bloodbank.io.Recordable;
 
-public abstract Person implements Recordcable {
-	//Declare
-	protected int nric;
-	protected String name;
-	protected String dob;
-	protected char gender;
-	protected int contact;
+public Person implements Recordcable {
+    //Declare
+    protected int nric;
+    protected String name;
+    protected String dob;
+    protected char gender;
+    protected int contact;
    
    //Normal constructor 
    public person(int nric, String name, int contact){
-   	this.nric = nric;
-   	this.name = name;
-   	this.gender = gender;
-   	this.dob = dob;
-   	this.contact = contact;
+    this.nric = nric;
+    this.name = name;
+    this.gender = gender;
+    this.dob = dob;
+    this.contact = contact;
    }
    //Setter
    public void setNRIC(int nric){
-   	this.nric = nric;
+    this.nric = nric;
    }
    public void setName(String name){
-   	this.name = name;
+    this.name = name;
    }
    public void setDob(String dob){
-   	this.dob = dob;
+    this.dob = dob;
    }
    public void setGender(char gender){
-   	this.gender = gender;
+    this.gender = gender;
    }
    public void setContact(int contact){
-   	this.contact = contact;
+    this.contact = contact;
    }
    //Getter
    public int getNRIC(){
-   	return nric;
+    return nric;
    }
    public String getName(){
-   	return name;
+    return name;
    }
    public String getDob(){
-   	return dob;
+    return dob;
    }
    public char getGender(){
-   	return gender;
+    return gender;
    }
    public int getContact(){
-   	return contact;
+    return contact;
    }
-   
-     public String toRecord();
+    public String toRecord() {
+      return nric + name + dob + gender + contact;
+    }
  }
  
