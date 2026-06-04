39d1cac7f72c6736c928fb9580e2e14ba7247c8d
shahxvi <2025171523@student.uitm.edu.my>
2026-06-05 00:01:51 +0800

    changes

--- src/main/java/com/bloodbank/transfusion/Bag.java
+++ src/main/java/com/bloodbank/transfusion/Bag.java
@@ -1,7 +1,49 @@
 // MIT LICENSE
 // Copyright (c) 2026 Marzell
+// Copyright (c) 2026 Shah
 
 package com.bloodbank.transfusion;
 
-public class Bag {
+import com.bloodbank.person.Donor;
+import com.bloodbank.io.Recordable;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Bag implements Recordable {
+    private static final int SHELF_LIFE_DAYS = 42;
+    private Donor donor;
+    private LocalDateTime transfusionDateTime;
+
+    public Bag(Donor donor, LocalDateTime transfusionDateTime) {
+        this.donor = donor;
+        this.transfusionDateTime = transfusionDateTime;
+    }
+
+    public Donor getDonor() {
+        return donor;
+    }
+
+    public LocalDateTime getTransfusionDateTime() {
+        return transfusionDateTime;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return transfusionDateTime.plusDays(SHELF_LIFE_DAYS);
+    }
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(getExpiryDate());
+    }
+
+    @Override
+    public String toString() {
+    }
+
+    @Override
+    public String toRecord() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        return transfusionDateTime.format(formatter);
+    }
 }
