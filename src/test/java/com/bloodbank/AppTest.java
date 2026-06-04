21a4a9473038831f26f50e134c52366a895e3389
shahxvi <2025171523@student.uitm.edu.my>
2026-06-05 00:49:01 +0800

    Add Testing

---
+++ src/test/java/com/bloodbank/AppTest.java
@@ -0,0 +1,22 @@
+package com.bloodbank;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.bloodbank.transfusion.Blood;
+
+public class AppTest {
+    @Test
+    public void bloodTest() {
+        Blood blood = new Blood("A", 1);
+        assertEquals("A", blood.getGroup());
+        assertEquals("A", blood.getBloodAntibody());
+
+        assertEquals("B", blood.getPlasmaAntibody());
+
+        assertEquals(0.445, blood.getRedCellVolume());
+        assertEquals(0.545, blood.getPlasmaVolume());
+        assertEquals(0.01, blood.getPlateletVolume());
+    }
+}
